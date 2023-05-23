c318b8fc4ec7cdc36b240d1f921058b3a0368d49
artem <artem.repnikov.g@.gmail.com>
2023-05-23 14:01:58 +0300

    test

---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая на вход принимает число (N),
+ а на выходе показывает все четные числа от 1 до N
+ 5 -> 2,4
+ 8 -> 2,4,6,8
+ */
+
+ System.Console.WriteLine("Введите N: ");
+
+int s = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= s; i++) // или i <= s или i < s+1
+{
+    if (i % 2 == 0)
+    {
+        System.Console.Write($"{i} ");
+    }
+}
